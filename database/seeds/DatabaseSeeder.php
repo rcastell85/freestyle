86fdd1332ecb6e0249223103c53fd721bb7838aa
Alvaro-Ezequiel-Zalbaveascoba <ae.zalbaveascoba@gmail.com>
2019-08-05 06:25:49 -0300

    Factory de amigos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
          $this->call(UsersTableSeeder::class);
          $this->call(ProfilesTableSeeder::class);
          $this->call(PostTableSeeder::class);
+         $this->call(friendTableSeeder::class);
     }
 }
